aa67b17aecfeb8b720e5618e1d217931e99ffadd
Marcin Mandryk <js.mandrykmarcin@gmail.com>
2020-01-14 19:49:00 +0000

    Updates

---
+++ precache-manifest.27bc3f5b431d9f9ac49e6e2f7dfeb444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a630f0f3f403c9111efaf168ca3b23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3ed07398282aedc692",
+    "url": "/static/css/2.ceea6fac.chunk.css"
+  },
+  {
+    "revision": "e85621be691da3a41ae2",
+    "url": "/static/css/main.d7e03f46.chunk.css"
+  },
+  {
+    "revision": "5d3ed07398282aedc692",
+    "url": "/static/js/2.19e51746.chunk.js"
+  },
+  {
+    "revision": "e85621be691da3a41ae2",
+    "url": "/static/js/main.1062474d.chunk.js"
+  },
+  {
+    "revision": "60742affa8dcc2cf9c96",
+    "url": "/static/js/runtime-main.37f88b9b.js"
+  }
+]);
